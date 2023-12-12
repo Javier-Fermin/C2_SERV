865a42593af7c5ddc68c6c0701278228b257cd1c
PerezImanol <imanolperezmu@gmail.com>
2023-12-12 12:14:51 +0100

    Match entity equals, has code and 2 queries

--- src/java/entity/Match.java
+++ src/java/entity/Match.java
@@ -119,11 +119,24 @@ public class Match {
                 + ", league=" + league + ", plays=" + plays + "]";
     }
     
-    public boolean equals(Match m){
-        if(m instanceof Match){
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
 
-        }else
-        return false;
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Match)) {
+            return false;
+        }
+        Match other = (Match) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
     }
     
 }
